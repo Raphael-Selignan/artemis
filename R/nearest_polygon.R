68007b77f7484a41d82004092685d74a96bebf97
Raphael-Selignan <raphael.selignan@gmail.com>
2017-11-14 16:29:59 +0100

    nearest_polygon function

---
+++ R/nearest_polygon.R
@@ -0,0 +1,22 @@
+#' Returns the closes polygon to a long lat point
+#'
+#' @param banRmatrix the banR matrix given by address_geocode
+#' @param cadaster_sheet the corresponding cadaster sheet
+#'
+#' @return integer
+#' @import sf
+#' @export
+#'
+#' @examples \dontrun{
+#' nearest_polygon(geocode_address("5 rue d'Olivet 75007"),cadaster_sheet_75107)
+#' }
+nearest_polygon <- function(banRmatrix,cadaster_sheet){
+  point <- sf::st_point(c(banRmatrix$longitude, banRmatrix$latitude))
+  point <- sf::st_sfc(point, crs = 4326)
+  UTM_point <- st_transform(point, crs = 32748)
+  UMT_sheet <- cadaster_sheet %>%
+  st_transform(crs = 32748)
+  distance_matrix <- st_distance(b, matrice2)
+  nearest_polygon_reference <- which.min(distance_matrix)
+  return(nearest_polygon_reference)
+}
