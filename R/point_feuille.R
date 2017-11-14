268f177379de02732e9e2eedd4eac6191940a4bc
Raphael-Selignan <raphael.selignan@gmail.com>
2017-11-14 23:22:48 +0100

    Bug fixes and comments

--- R/point_feuille.R
+++ R/point_feuille.R
@@ -1,7 +1,7 @@
 #' Produces the reference of the feuille corresponding to the point
 #'
 #' @param banRmatrix matrix given out by banR
-#' @param cadaster_sheet_feuilles corresponding cadaster_sheet
+#' @param cadastre_sheet_feuilles corresponding cadaster_sheet in dataframe form
 #'
 #' @return integer
 #' @import sf
@@ -9,13 +9,24 @@
 #'
 #' @examples \dontrun{
 #' point_feuille(geocode_address("youradrress"),feuille_cadaster_sheet.json)}
-point_feuille <- function(banRmatrix,cadaster_sheet_feuilles){
+#'
+point_feuille <- function(banRmatrix,cadastre_sheet_feuilles){
+
+  ## Transforms the banRmatrix longitude & latitude into geographical objects with CRS
   point <- sf::st_point(c(banRmatrix$longitude, banRmatrix$latitude))
   point <- sf::st_sfc(point, crs = 4326)
-  UTM_point <- st_transform(point, crs = 32748)
-  UTM_sheet <- cadaster_sheet_feuilles %>%
-    st_transform(crs = 32748)
-  distance_matrix <- st_distance(UTM_point, UTM_sheet)
+
+  ## Changes CRS to go from longitude latitude to UTM to be able to compute distance both for point and matrix
+  UTM_point <- sf::st_transform(point, crs = 32748)
+  UTM_sheet <- cadastre_sheet_feuilles %>%
+    sf::st_transform(crs = 32748)
+
+  ## Evaluate distance between the point from the addres and every feuille area in the downloaded dataframe
+  distance_matrix <- sf::st_distance(UTM_point, UTM_sheet)
+
+  ## Chososes the feuille closest (containing) to the point
   point_feuille_reference <- which.min(distance_matrix)
+
+  ## Returns its row number
   return(point_feuille_reference)
 }
